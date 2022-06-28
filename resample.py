f7af423be2ce272ae002dffa366a00c5f37b89ed
sneguraw <sneguraw@gmail.com>
2022-06-28 23:27:42 +0300

    resample add log

--- resample.py
+++ resample.py
@@ -1,86 +1,72 @@
 from osgeo import gdal, osr
 import argparse
-
 import math
 import os
-
-def main(input_tif, output_tif, pixel_size, epsg):
-
-    # если проекция не указана пользователем, то используем поумолчанию 3857 и рассчитываем размер
-    # пикселя в "реальных метрах"
-    if epsg == None:
-        epsg = '3857'
-        dataset = gdal.Open(input_tif)
-
-        #get center rastr
-        xmin, ymin, xmax, ymax = GetExtent(dataset)
-        center = [(xmin + xmax) / 2, (ymin + ymax) / 2]
-
-        # input SpatialReference
-        proj = osr.SpatialReference(wkt=dataset.GetProjection())
-        epsgid = int(proj.GetAttrValue('AUTHORITY',1))
-        inSpatialRef = osr.SpatialReference()
-        inSpatialRef.ImportFromEPSG(epsgid)
-
-        # output SpatialReference
-        outSpatialRef = osr.SpatialReference()
-        outSpatialRef.ImportFromEPSG(4326)
-        # trasform  rastr cernter to EPSG:4326
-        re_center = ReprojectCoords(center, src_srs=inSpatialRef, tgt_srs=outSpatialRef)
-        # Get coefficient of real meter
-        coef = math.cos(math.radians(re_center[0]))
-        print(coef)
-        # change pixel size
-        pixel_size = pixel_size * coef
+import logging
+import datetime
+import os
 
 
-    gdal.Warp(output_tif, input_tif, xRes=pixel_size, yRes=pixel_size, dstSRS='EPSG:' + epsg)
+def main(input_tif, output_tif, pixel_size, dst_crs):
+    """If user projection is "None" - use projection "3857", and calculate pixel size in 'real meter'."""
+    if dst_crs is None:
+        dst_crs = '3857'
+        pixel_size = calculate_res_from_gsd(input_tif, pixel_size)
+
+    gdal.Warp(output_tif, input_tif, xRes=pixel_size, yRes=pixel_size, dstSRS='EPSG:' + dst_crs)
     print('End resample. Result:', output_tif)
 
-def GetExtent(ds):
-    "Get extent of dataset raster"
+
+def calculate_res_from_gsd(input_tif, pixel_size):
+    # open tif
+    dataset = gdal.Open(input_tif)
+    # get center rastr
+    xmin, ymin, xmax, ymax = get_extent(dataset)
+    center = [(xmin + xmax) / 2, (ymin + ymax) / 2]
+    # input SpatialReference
+    proj = osr.SpatialReference(wkt=dataset.GetProjection())
+    epsgid = int(proj.GetAttrValue('AUTHORITY', 1))
+    inSpatialRef = osr.SpatialReference()
+    inSpatialRef.ImportFromEPSG(epsgid)
+    # output SpatialReference
+    outSpatialRef = osr.SpatialReference()
+    outSpatialRef.ImportFromEPSG(4326)
+    # trasform  rastr cernter to EPSG:4326
+    re_center = reproject_coords(center, src_srs=inSpatialRef, tgt_srs=outSpatialRef)
+    # Get coefficient of real meter
+    coef = math.cos(math.radians(re_center[0]))
+    # change pixel size
+    pixel_size = pixel_size * coef
+    return pixel_size
+
+def get_extent(ds):
+    """Get extent of dataset raster"""
     xmin, xpixel, _, ymax, _, ypixel = ds.GetGeoTransform()
     width, height = ds.RasterXSize, ds.RasterYSize
     xmax = xmin + width * xpixel
     ymin = ymax + height * ypixel
     return xmin, ymin, xmax, ymax
 
-def ReprojectCoords(coords,src_srs,tgt_srs):
-    "Reprojection coordinates"
+def reproject_coords(coords,src_srs,tgt_srs):
+    """Reprojection coordinates"""
     transform = osr.CoordinateTransformation(src_srs, tgt_srs)
     x, y, z = transform.TransformPoint(coords[0], coords[1])
     return x, y
 
-def TransformOnePixel(dataset, output_tif):
-    "!!!Это теперь не нужно, но пусть пока тут полежит вруг чё"
-
-    "создаем растр из одного пикселя с праметрами пикселя основного растра"
-
-    #print("Raster size x:", dataset.RasterXSize, "y:", dataset.RasterYSize)
-    xcenter_poi = dataset.RasterXSize // 2
-    ycenter_poi = dataset.RasterYSize // 2
-
-    pixtif = os.path.dirname(output_tif) + "/1pix.tif"
-    output_pixtif =os.path.dirname(output_tif) + "/1pix_proj.tif"
-
-    # вырезаем один пиксель из исходного растра
-    raster_1pix = gdal.Translate(pixtif, dataset, srcWin=[xcenter_poi, ycenter_poi, 1, 1])
-    raster_1pix = None
-    # перепроицирование пикселя
-    warp = gdal.Warp(output_pixtif, pixtif, dstSRS='EPSG:3857')
-    warp = None
-
-    ds = gdal.Open(output_pixtif)
-    x, xsize, xrot, y, yrot, ysize = ds.GetGeoTransform()
-    return xsize, ysize
-
-
 if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input_tif', type=str, help='input .tif file')
-    parser.add_argument('output_tif', type=str, help='output .tif file')
-    parser.add_argument('--pixel_size', type=float, help='pixel size in meter', default=0.8)
-    parser.add_argument('--epsg', type=str, help='EPSG', default=None)
-    args = parser.parse_args()
-    main(args.input_tif, args.output_tif, args.pixel_size, args.epsg)
+    dir = os.path.dirname(os.path.abspath(__file__))
+    logging.basicConfig(filename= dir + "/resample_mapflow.log", level=logging.INFO)
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('input_tif', type=str, help='input .tif file')
+        parser.add_argument('output_tif', type=str, help='output .tif file')
+        parser.add_argument('--pixel_size', type=float, help='pixel size in meter', default=0.8)
+        parser.add_argument('--dst_crs', type=str, help='EPSG', default=None)
+        args = parser.parse_args()
+        logging.info(str(datetime.datetime.now()) + ' Start resample. Input: ' + args.input_tif)
+        main(args.input_tif, args.output_tif, args.pixel_size, args.dst_crs)
+        logging.info(str(datetime.datetime.now()) + ' End resample. Result:' + args.output_tif)
+    except Exception as e:
+        er = str(datetime.datetime.now()) + ' ' + str(e)
+        logging.error(er)
+        print(er)
